8e03553ce010496d534853e36fc2968aa85f6eca
fmnihal <fattahmahmudnihal@gmail.com>
2025-09-25 05:20:11 +0600

    card container, conditional rendering between available and selected players, toggle buttons

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -3,41 +3,40 @@ import { User, Flag } from 'lucide-react';
 
 const AvailablePlayers=({playersPromise})=>{
     const playersData= use(playersPromise);
-    console.log(playersData);
+    // console.log(playersData);
     return(
-        <div className='max-w-5/6 mx-auto'>
+        <div className='max-w-5/6 mx-auto grid justify-items-center md:grid-cols-2 lg:grid-cols-3 gap-5'>
             {
                 playersData.map(player=>{
                     return(
-                        <div className="card bg-base-100 w-96 shadow-sm p-4">
-                        <figure>
-                            <img
-                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                            alt="Shoes" />
+                        <div className="card bg-base-100 w-72 shadow-sm p-4">
+                        <figure className='h-40 overflow-hidden'>
+                            <img className='w-full h-full object-cover object-top'
+                            src={player.playerImg}
+                            alt={player.playerName} />
                         </figure>
                         <div className="card-body">
-                            <h2 className="card-title"><User /> Viral Kohli</h2>
-                            <div className='flex justify-between border-b-1 border-gray-400 pb-2'>
+                            <h2 className="card-title"><User /> {player.playerName}</h2>
+                            <div className='flex flex-wrap justify-between items-center border-b-1 border-gray-400 pb-2'>
                                 <div className='flex'>
-                                    <Flag></Flag><span className='ml-1'>India</span>
+                                    <Flag></Flag><span className='ml-1'>{player.playerCountry}</span>
                                 </div>
-                                <button className='btn btn-soft'>Allrounder</button>
+                                <button className='btn btn-soft'>{player.playerRole}</button>
                             </div>
                             <div>
                                 <div className='flex justify-between'>
                                     <span className='font-bold'>Rating</span>
-                                    <span>5</span>
+                                    <span>{player.rating}</span>
                                 </div>
                                 <div className='flex justify-between'>
-                                    <span className='font-bold'>Left Hand Bat</span>
-                                    <span>Right Hand Bowl</span>
+                                    <span className='font-bold'>{player.battingStyle}</span>
+                                    <span>{player.bowlingStyle}</span>
                                 </div>
                             </div>
 
                             <div className='flex justify-between items-center'>
                                 <div>
-                                    <span className='font-bold mr-1'>Price: $</span>
-                                    <span>15000000</span>
+                                    <span className='font-bold mr-1'>Price: </span><span><b>$</b>{player.price}</span>
                                 </div>
                                 <div className="card-actions">
                                     <button className="btn">Choose Player</button>
